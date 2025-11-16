436b1060d4c19889a67ceb4b6aa8bb1d195aecaa
Jhonne <jonee4339@hotmail.com>
2025-11-15 22:30:20 -0300

    added new image and requiriemnmt file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from click import prompt
 import streamlit as st
 import google.generativeai as genai
 
@@ -10,7 +11,20 @@ def ask_and_get_answer(prompt, img):
     response = model.generate_content([prompt, img])
     return response.text
 
-if __name__ == "__name__":
-    load_dotenv(find_dotenv(), override=True)
-    genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
+def st_image_to_pil(st_image):
+    import io
+    from PIL import Image
 
+
+load_dotenv(find_dotenv(), override=True)
+genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+
+st.image("logo.svg", width=75)
+st.subheader("Talking with an Image")
+# st.image()
+img = st.file_uploader("Select an image: ", type=["jpg", "jpeg", "png", "gif"])
+if img:
+    st.image(img, caption="Talk with this image.")
+
+    prompt = st.text_area("Ask a question about this image: ")
+    if prompt:
