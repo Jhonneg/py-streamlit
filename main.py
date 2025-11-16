61891b35f03a432e6d8a5881d07f880ea7fd0d17
Jhonne <jonee4339@hotmail.com>
2025-11-15 22:59:31 -0300

    project finished

--- main.py
+++ main.py
@@ -11,15 +11,20 @@ def ask_and_get_answer(prompt, img):
     response = model.generate_content([prompt, img])
     return response.text
 
+
 def st_image_to_pil(st_image):
     import io
     from PIL import Image
+
+    image_data = st_image.read()
+    pil_image = Image.open(io.BytesIO(image_data))
+    return pil_image
 
 
 load_dotenv(find_dotenv(), override=True)
 genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
 
-st.image("logo.svg", width=75)
+# st.image("logo.svg", width=75)
 st.subheader("Talking with an Image")
 # st.image()
 img = st.file_uploader("Select an image: ", type=["jpg", "jpeg", "png", "gif"])
@@ -28,3 +33,18 @@ if img:
 
     prompt = st.text_area("Ask a question about this image: ")
     if prompt:
+        pil_image = st_image_to_pil(img)
+        with st.spinner("Running..."):
+            answer = ask_and_get_answer(prompt, pil_image)
+            st.text_area("Gemini Answer: ", value=answer)
+
+        st.divider()
+        if "history" not in st.session_state:
+            st.session_state.history = ""
+
+        value = f"Q: {prompt} \n\n A: {answer}"
+        st.session_state.history = (
+            f"{value} \n\n {'-' * 100} \n\n {st.session_state.history}"
+        )
+        h = st.session_state.history
+        st.text_area(label="Chat history", value=h, height=400, key="history")
